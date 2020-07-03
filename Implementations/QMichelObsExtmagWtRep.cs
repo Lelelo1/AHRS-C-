4c1ac74f075202f4450e508e182e0b3333975dbc
Lelelo1 <lelelo1@Leos-Air.lan>
2020-07-03 14:55:51 +0200

    Add Implementations folder

---
+++ Implementations/QMichelObsExtmagWtRep.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Spatial.Euclidean;
+// https://github.com/tyrex-team/benchmarks-attitude-smartphones/blob/master/src/Filters/Implementations/QMichelObsExtmagWtRep.m
+namespace Logic.Ahrs.Magnetometer.Tyrex.Implementations
+{
+    public class QMichelObsExtmagWtRep : AttitudeFilter
+    {
+
+        public 
+
+        double MagNormThreshold { get; set; } = 15;
+        double Beta { get; set; } = 0.3;
+        double Ka { get; set; } = 2;
+        double Km { get; set; } = 1;
+
+        // oldValues = [];
+        double LastMagPerturbation { get; set; }
+        // magUpdate;
+
+        double TimeMagNopert { get; set; } = 2;
+        double TimeToSavePreviousData { get; set; } = 3;
+
+        public override Quaternion Update(Vector<double> gyr, Vector<double> acc, Vector<double> mag, double dT)
+        {
+            
+        }
+    }
+}
