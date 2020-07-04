014bcb6fc964c430c9b54d76224e70e7bec868bf
Lelelo1 <lelelo1@Leos-Air.lan>
2020-07-04 09:40:20 +0200

    translating

--- Implementations/QMichelObsExtmagWtRep.cs
+++ Implementations/QMichelObsExtmagWtRep.cs
@@ -7,8 +7,6 @@ namespace Logic.Ahrs.Magnetometer.Tyrex.Implementations
     public class QMichelObsExtmagWtRep : AttitudeFilter
     {
 
-        public 
-
         double MagNormThreshold { get; set; } = 15;
         double Beta { get; set; } = 0.3;
         double Ka { get; set; } = 2;
@@ -16,14 +14,37 @@ namespace Logic.Ahrs.Magnetometer.Tyrex.Implementations
 
         // oldValues = [];
         double LastMagPerturbation { get; set; }
-        // magUpdate;
+        bool MagUpdate { get; set; }
 
         double TimeMagNopert { get; set; } = 2;
         double TimeToSavePreviousData { get; set; } = 3;
 
+        public QMichelObsExtmagWtRep()
+        {
+            LastMagPerturbation = TimeMagNopert;
+            MagUpdate = false;
+        }
+
         public override Quaternion Update(Vector<double> gyr, Vector<double> acc, Vector<double> mag, double dT)
         {
+            var magUpdate = Math.Abs(mag.L2Norm() - MagRefNorm) < MagNormThreshold;
+
+
+            // % Do not consider a magUpdate for next [timeMagNopert] seconds if a ~magUpdate is detected
+            LastMagPerturbation = MagUpdate ? LastMagPerturbation + dT : LastMagPerturbation;
+            magUpdate = LastMagPerturbation < TimeMagNopert ? false : magUpdate;
+
+            // % If we detect a perturbation, we replay all previous values from current time - [timeToSavePreviousData]
+            // % to current time without magnetic field updates
+
             
+            if (MagUpdate && !magUpdate)
+            {
+                var tmpBeta = Beta;
+                Beta = 2;
+                Quaternion = 
+            }
+
         }
     }
 }
